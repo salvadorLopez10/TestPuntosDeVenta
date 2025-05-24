44897c4166f771534681c5eed467a451f8b56d05
Salvador Lopez <jslb_cafcb10@hotmail.com>
2025-05-23 20:24:03 -0600

    Se lee en variable de ambiente el la URL del API

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5258';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:5258/api',
+  baseURL: `${API_BASE_URL}/api`
 });
